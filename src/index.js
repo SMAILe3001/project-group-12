fbd20669a437bb5cab683280b24300118f318da2
SMAILe3001 <smaile3001@gmail.com>
2023-04-13 18:21:08 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import API from './js/popular/load-popular';
 import RMP from './js/popular/render-markup-popular';
 
-API.loadPopular()
-  .then(RMP.renderMarkup)
-  .catch(() => {
-    console.log('error');
-  });
+API.loadPopular().then(RMP.renderMarkup);
+// .catch(() => {
+//   console.log('error');
+// });
