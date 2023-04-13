9869fdf9aeb702290d9beb3e34640b8677983ffc
Konstantin Leschenko <klav28@gmail.com>
2023-04-13 21:11:33 +0300

    Update load-popular.js

--- src/js/popular/load-popular.js
+++ src/js/popular/load-popular.js
@@ -1,13 +1,12 @@
 const API_KEY = "ce2d3c43772ebdf360cfaed86f3ba1bf";
 
-function loadPopular() { 
+async function loadPopular() { 
     const url = `https://api.themoviedb.org/3/trending/movie/day?api_key=${API_KEY}`;
-    return fetch(url).then(response => {
-        if (response.ok) {
-            return response.json()
-        } throw new Error(response.statusText)
-    } 
-)
+    const response = await fetch(url);
+    if (response.ok) {
+        return response.json();
+    }
+    throw new Error(response.statusText);
 };
 
 export default {loadPopular}
