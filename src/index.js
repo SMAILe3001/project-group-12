79c3c341e82e2f1ad41ab261e743554a3a13e8b3
SMAILe3001 <smaile3001@gmail.com>
2023-04-13 18:13:05 +0300

    fix

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
-import unsplashAPI from './js/unsplasu-api';
-unsplashAPI();
+import API from './js/popular/load-popular';
+import RMP from './js/popular/render-markup-popular';
+
+API.loadPopular()
+  .then(RMP.renderMarkup)
+  .catch(() => {
+    console.log('error');
+  });
--- src/js/popular/render-markup-popular.js
+++ src/js/popular/render-markup-popular.js
@@ -1,28 +1,26 @@
-import API from './js/popular/load-popular';
-
 const refs = {
-    galleryFilms: document.querySelector('.galeryFilms-js'),
-}
+  galleryFilms: document.querySelector('.galeryFilms-js'),
+};
 
-console.log(refs.galleryFilms)
-
-API.loadPopular()
-    .then(renderMarkup)
-    // .catch(() => { console.log("error") })
+console.log(refs.galleryFilms);
 
 function renderMarkup(films) {
-    console.log(films.results)
-    const createdElements = films.results.map(film => {
-        cardFilm = `
+  console.log(films.results);
+  const createdElements = films.results
+    .map(film => {
+      cardFilm = `
         <div>
             <img src="https://image.tmdb.org/t/p/w500${film.poster_path}" alt="poster">
             <h2>${film.original_title}</h2>
             <p>${film.media_type}</p>
             <p>${film.release_date}</p>
         </div>
-        `
-        return cardFilm
-    }).join('') 
+        `;
+      return cardFilm;
+    })
+    .join('');
 
-    refs.galleryFilms.innerHTML = createdElements
+  refs.galleryFilms.innerHTML = createdElements;
 }
+
+export default { renderMarkup };
