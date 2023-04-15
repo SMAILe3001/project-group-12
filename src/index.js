d4488a4d10894678380d4656708655803ae0cfda
klav28 <klav28@gmail.com>
2023-04-15 16:18:29 +0300

    added modal interface

--- src/index.js
+++ src/index.js
@@ -5,4 +5,8 @@ import error from './js/popular/on-fetch-error';
 import APIF from './js/form_search/load-form';
 import RMF from './js/form_search/render-murkap-form';
 
+import modalInfoFilmShow from './js/modal-info-film';
+
 API.loadPopular().then(RMP.renderMarkup).catch(error);
+
+modalInfoFilmShow();
