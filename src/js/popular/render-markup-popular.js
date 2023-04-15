5679f51e2ff8f369a6f73076e425dbf988d97af6
SMAILe3001 <smaile3001@gmail.com>
2023-04-15 15:54:30 +0300

    genRes

--- src/js/popular/render-markup-popular.js
+++ src/js/popular/render-markup-popular.js
@@ -1,24 +1,5 @@
-const genres = {
-  28: 'Action',
-  12: 'Adventure',
-  16: 'Animation',
-  35: 'Comedy',
-  80: 'Crime',
-  99: 'Documentary',
-  18: 'Drama',
-  10751: 'Family',
-  14: 'Fantasy',
-  36: 'History',
-  27: 'Horror',
-  10402: 'Music',
-  9648: 'Mystery',
-  10749: 'Romance',
-  878: 'Science Fiction',
-  10770: 'TV Movie',
-  53: 'Thriller',
-  10752: 'War',
-  37: 'Western',
-};
+import genres from '../genres';
+
 const refs = {
   galleryFilms: document.querySelector('.galeryFilms-js'),
 };
@@ -30,26 +11,33 @@ function renderMarkup(films) {
       if (film.genre_ids.length <= 2) {
         const cardFilm = `
       <div class="film-card">
-        <img class="film-poster" src="https://image.tmdb.org/t/p/w500${film.poster_path}" alt="poster">
+        <img class="film-poster" src="https://image.tmdb.org/t/p/w500${
+          film.poster_path
+        }" alt="poster">
         <h2 class="film-title">${film.original_title}</h2>
         <div class="film-info">
-        <span class="film-details">${film.genre_ids.map(id => genres[id]).join(', ')} | ${film.release_date.substr(0, 4)}</span>
+        <span class="film-details">${film.genre_ids
+          .map(id => genres[id])
+          .join(', ')} | ${film.release_date.substr(0, 4)}</span>
         </div>
-      </div>`
+      </div>`;
         return cardFilm;
       } else {
         const cardFilm = `
       <div class="film-card">
-        <img class="film-poster" src="https://image.tmdb.org/t/p/w500${film.poster_path}" alt="poster">
+        <img class="film-poster" src="https://image.tmdb.org/t/p/w500${
+          film.poster_path
+        }" alt="poster">
         <h2 class="film-title">${film.original_title}</h2>
         <div class="film-info">
-        <span class="film-details">${film.genre_ids.slice(0, 2).map(id => genres[id]).join(', ')}, Other | ${film.release_date.substr(0, 4)}</span>
+        <span class="film-details">${film.genre_ids
+          .slice(0, 2)
+          .map(id => genres[id])
+          .join(', ')}, Other | ${film.release_date.substr(0, 4)}</span>
         </div>
-      </div>`
+      </div>`;
         return cardFilm;
       }
-      
-      
     })
     .join('');
 
