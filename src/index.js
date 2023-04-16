239732db5e240363f6a91b7da6e3e85730a17234
Oleksandr <elvenmonuskrypt@gmail.com>
2023-04-16 16:19:45 +0300

    add tui-pagination to the project
    підключено бібліотеку.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import API from './js/popular/load-popular';
 import RMP from './js/popular/render-markup-popular';
 import error from './js/popular/on-fetch-error';
-
+import PGN from './js/pagination';
 import APIF from './js/form_search/load-form';
 import RMF from './js/form_search/render-murkap-form';
 
