83556cbb06b3e7702421c66c23c929e6dbc036e1
klav28 <klav28@gmail.com>
2023-04-17 22:08:08 +0300

    markup & func update

--- src/js/modal-info-film.js
+++ src/js/modal-info-film.js
@@ -15,30 +15,44 @@ async function loadFilmInfo(movieID) {
 }
 
 function handleAddToQueue() {
-  //console.log('Add To Queue ' + filmID);
-  local.Queue.addOrRemove(filmID);
-  console.log(local.Queue.value);
+    local.Queue.addOrRemove(filmID);
+    console.log(local.Queue.value);
+    refs.btnAddToQueue.classList.toggle('modal-button__active');
 }
 
 function handleAddToWatched() {
   //console.log('Add To Watching ' + filmID);
   local.Watched.addOrRemove(filmID);
-  console.log(local.Watched.value);
+    console.log(local.Watched.value);
+    refs.btnAddToWatched.classList.toggle('modal-button__active');
 }
 
 function handleCloseModal() {
-  refs.backdrop.classList.add('is-hidden');
-  refs.btnAddToWatched.removeEventListener('click', handleAddToWatched);
-  refs.btnAddToQueue.removeEventListener('click', handleAddToQueue);
-  refs.filmInfoCardImage.innerHTML = '';
-  refs.filmInfoCardBloque.innerHTML = '';
+    refs.backdrop.classList.add('is-hidden');
+    refs.btnAddToWatched.removeEventListener('click', handleAddToWatched);
+    refs.btnAddToQueue.removeEventListener('click', handleAddToQueue);
+    refs.filmInfoCardImage.innerHTML = '';
+    refs.filmInfoCardBloque.innerHTML = '';
+    window.removeEventListener('keydown', handleKeydown);
+    
 }
 
 function renderFilmInfo(film) {
-  console.log(film);
-  refs.filmInfoCardImage.innerHTML = `<img class="film-info-card__poster" src="https://image.tmdb.org/t/p/w500${film.poster_path}" alt="poster">`;
-  film.vote_average = Math.round(film.vote_average * 10) / 10;
-  refs.filmInfoCardBloque.innerHTML = filmDescriptionCreate(film);
+    console.log(film);
+    // дописать дефолтный постер если его нет
+    refs.filmInfoCardImage.innerHTML = `<img class="film-info-card__poster" src="https://image.tmdb.org/t/p/w500${film.poster_path}" alt="poster">`;
+    film.vote_average = Math.round(film.vote_average * 10) / 10;
+    refs.filmInfoCardBloque.innerHTML = filmDescriptionCreate(film);
+    if (local.Queue.exist(filmID)) {
+        refs.btnAddToQueue.classList.add('modal-button__active');
+    } else {
+        refs.btnAddToQueue.classList.remove('modal-button__active');
+    }
+    if (local.Watched.exist(filmID)) {
+        refs.btnAddToWatched.classList.add('modal-button__active');
+    } else {
+        refs.btnAddToWatched.classList.remove('modal-button__active');
+    }
 }
 
 function handleFilmCardClick(ev) {
@@ -49,14 +63,27 @@ function handleFilmCardClick(ev) {
     refs.modalClose.addEventListener('click', handleCloseModal);
     refs.btnAddToQueue.addEventListener('click', handleAddToQueue);
     refs.btnAddToWatched.addEventListener('click', handleAddToWatched);
-    //            refs.backdrop.addEventListener("click", handleCloseModal);
+    window.addEventListener('keydown', handleKeydown);      
+    refs.backdrop.addEventListener("click", handleBackdropClick);
 
     loadFilmInfo(filmID).then(renderFilmInfo);
   }
 }
 
+function handleKeydown(ev) {
+    if (ev.code === 'Escape') {
+        handleCloseModal();
+    }
+}
+
+function handleBackdropClick(ev) {
+    if (ev.target === refs.backdrop) {
+        handleCloseModal();
+    };
+}
+
 function modalInfoFilmShow() {
-  refs.galleryFilms.addEventListener('click', handleFilmCardClick);
+    refs.galleryFilms.addEventListener('click', handleFilmCardClick);
 }
 
 modalInfoFilmShow();
