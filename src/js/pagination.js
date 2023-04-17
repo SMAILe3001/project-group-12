ae37bcca0e2171ddfb9079d716b8f27c04b72745
SMAILe3001 <smaile3001@gmail.com>
2023-04-17 10:41:15 +0300

    pagination
    переробив пагінацію на функцію

--- src/js/pagination.js
+++ src/js/pagination.js
@@ -1,34 +1,37 @@
 import Pagination from 'tui-pagination';
 import 'tui-pagination/dist/tui-pagination.css';
 import API from './popular/load-popular';
-import apiSearchForm from './form_search/load-form'
-
-const container = document.getElementById('tui-pagination-container');
+import apiSearchForm from './form_search/load-form';
+import { refs } from './refs';
 
 const options = {
-    totalItems: 20000,
-    itemsPerPage: 20,
-    visiblePages: 7,
-    page: 1,
-    centerAlign: true,
-    firstItemClassName: 'tui-first-child',
-    lastItemClassName: 'tui-last-child',
-    template: {
-      page: '<a href="https://api.themoviedb.org/3/trending/movie/day?page={{page}}" class="tui-page-btn">{{page}}</a>',
-      currentPage: '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
-      moveButton:
-        '<a href="https://api.themoviedb.org/3/trending/movie/day?page={{page}}" class="tui-page-btn tui-{{type}}">' +
-          '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</a>',
-      disabledMoveButton:
-        '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
-          '<span class="tui-ico-{{type}}">{{type}}</span>' +
-        '</span>',
-      moreButton:
-        '<a href="https://api.themoviedb.org/3/trending/movie/day?page={{page}}" class="tui-page-btn tui-{{type}}-is-ellip">' +
-          '<span class="tui-ico-ellip">...</span>' +
-        '</a>'
-    }
-  };
-  
-  const pagination = new Pagination(container, options);
+  totalItems: 20000,
+  itemsPerPage: 20,
+  visiblePages: 7,
+  page: 1,
+  centerAlign: true,
+  firstItemClassName: 'tui-first-child',
+  lastItemClassName: 'tui-last-child',
+  template: {
+    page: '<a href="https://api.themoviedb.org/3/trending/movie/day?page={{page}}" class="tui-page-btn">{{page}}</a>',
+    currentPage:
+      '<strong class="tui-page-btn tui-is-selected">{{page}}</strong>',
+    moveButton:
+      '<a href="https://api.themoviedb.org/3/trending/movie/day?page={{page}}" class="tui-page-btn tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</a>',
+    disabledMoveButton:
+      '<span class="tui-page-btn tui-is-disabled tui-{{type}}">' +
+      '<span class="tui-ico-{{type}}">{{type}}</span>' +
+      '</span>',
+    moreButton:
+      '<a href="https://api.themoviedb.org/3/trending/movie/day?page={{page}}" class="tui-page-btn tui-{{type}}-is-ellip">' +
+      '<span class="tui-ico-ellip">...</span>' +
+      '</a>',
+  },
+};
+
+export function paginationRender(totalItems) {
+  options.totalItems = totalItems;
+  const pagination = new Pagination(refs.container, options);
+}
