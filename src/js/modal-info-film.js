9e9126ee90fcc99153da3878962ad7f1368f39d6
BubblePenguin <kotov.senpai@gmail.com>
2023-04-18 15:56:43 +0300

    bruh it just must have.. its unusible without this changes

--- src/js/modal-info-film.js
+++ src/js/modal-info-film.js
@@ -16,46 +16,61 @@ async function loadFilmInfo(movieID) {
 }
 
 function handleAddToQueue() {
-    local.Queue.addOrRemove(filmID);
-    console.log(local.Queue.value);
-    refs.btnAddToQueue.classList.toggle('modal-button__active');
+  local.Queue.addOrRemove(filmID);
+  console.log(local.Queue.value);
+  refs.btnAddToQueue.classList.toggle('modal-button__active');
+  if (local.Queue.exist(filmID)) {
+    refs.btnAddToQueue.textContent = 'Remove From Queue';
+  } else {
+    refs.btnAddToQueue.textContent = 'Add to Queue';
+  }
 }
 
 function handleAddToWatched() {
   //console.log('Add To Watching ' + filmID);
   local.Watched.addOrRemove(filmID);
-    console.log(local.Watched.value);
-    refs.btnAddToWatched.classList.toggle('modal-button__active');
+  console.log(local.Watched.value);
+  refs.btnAddToWatched.classList.toggle('modal-button__active');
+  if (local.Watched.exist(filmID)) {
+    refs.btnAddToWatched.textContent = 'Remove From Watched';
+  } else {
+    refs.btnAddToWatched.textContent = 'Add to Watched';
+  }
 }
 
 function handleCloseModal() {
-    refs.backdrop.classList.add('is-hidden');
-    refs.btnAddToWatched.removeEventListener('click', handleAddToWatched);
-    refs.btnAddToQueue.removeEventListener('click', handleAddToQueue);
-    refs.filmInfoCardImage.innerHTML = '';
-    refs.filmInfoCardBloque.innerHTML = '';
-    window.removeEventListener('keydown', handleKeydown);
-    
+  refs.backdrop.classList.add('is-hidden');
+  refs.btnAddToWatched.removeEventListener('click', handleAddToWatched);
+  refs.btnAddToQueue.removeEventListener('click', handleAddToQueue);
+  refs.filmInfoCardImage.innerHTML = '';
+  refs.filmInfoCardBloque.innerHTML = '';
+  window.removeEventListener('keydown', handleKeydown);
 }
 
 function renderFilmInfo(film) {
-    console.log(film);
-    // дописать дефолтный постер если его нет
-    refs.filmInfoCardImage.innerHTML = `<img class="film-info-card__poster" src="${film.poster_path === null
-        ? `${imgUrl}`
-        : `https://image.tmdb.org/t/p/w500${film.poster_path}`}" alt="poster">`;
-    film.vote_average = Math.round(film.vote_average * 10) / 10;
-    refs.filmInfoCardBloque.innerHTML = filmDescriptionCreate(film);
-    if (local.Queue.exist(filmID)) {
-        refs.btnAddToQueue.classList.add('modal-button__active');
-    } else {
-        refs.btnAddToQueue.classList.remove('modal-button__active');
-    }
-    if (local.Watched.exist(filmID)) {
-        refs.btnAddToWatched.classList.add('modal-button__active');
-    } else {
-        refs.btnAddToWatched.classList.remove('modal-button__active');
-    }
+  console.log(film);
+  // дописать дефолтный постер если его нет
+  refs.filmInfoCardImage.innerHTML = `<img class="film-info-card__poster" src="${
+    film.poster_path === null
+      ? `${imgUrl}`
+      : `https://image.tmdb.org/t/p/w500${film.poster_path}`
+  }" alt="poster">`;
+  film.vote_average = Math.round(film.vote_average * 10) / 10;
+  refs.filmInfoCardBloque.innerHTML = filmDescriptionCreate(film);
+  if (local.Queue.exist(filmID)) {
+    refs.btnAddToQueue.classList.add('modal-button__active');
+    refs.btnAddToQueue.textContent = 'Remove From Queue';
+  } else {
+    refs.btnAddToQueue.classList.remove('modal-button__active');
+    refs.btnAddToQueue.textContent = 'Add to Queue';
+  }
+  if (local.Watched.exist(filmID)) {
+    refs.btnAddToWatched.classList.add('modal-button__active');
+    refs.btnAddToWatched.textContent = 'Remove From Watched';
+  } else {
+    refs.btnAddToWatched.classList.remove('modal-button__active');
+    refs.btnAddToWatched.textContent = 'Add to Watched';
+  }
 }
 
 function handleFilmCardClick(ev) {
@@ -66,27 +81,27 @@ function handleFilmCardClick(ev) {
     refs.modalClose.addEventListener('click', handleCloseModal);
     refs.btnAddToQueue.addEventListener('click', handleAddToQueue);
     refs.btnAddToWatched.addEventListener('click', handleAddToWatched);
-    window.addEventListener('keydown', handleKeydown);      
-    refs.backdrop.addEventListener("click", handleBackdropClick);
+    window.addEventListener('keydown', handleKeydown);
+    refs.backdrop.addEventListener('click', handleBackdropClick);
 
     loadFilmInfo(filmID).then(renderFilmInfo);
   }
 }
 
 function handleKeydown(ev) {
-    if (ev.code === 'Escape') {
-        handleCloseModal();
-    }
+  if (ev.code === 'Escape') {
+    handleCloseModal();
+  }
 }
 
 function handleBackdropClick(ev) {
-    if (ev.target === refs.backdrop) {
-        handleCloseModal();
-    };
+  if (ev.target === refs.backdrop) {
+    handleCloseModal();
+  }
 }
 
 function modalInfoFilmShow() {
-    refs.galleryFilms.addEventListener('click', handleFilmCardClick);
+  refs.galleryFilms.addEventListener('click', handleFilmCardClick);
 }
 
 modalInfoFilmShow();
