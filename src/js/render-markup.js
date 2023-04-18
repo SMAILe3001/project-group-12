8b6acdc8904a72499753ce45512c4faeaed3917d
Stanislav Pravdiuk <stanislav.pravdiuk@gmail.com>
2023-04-18 15:58:51 +0300

    fix my bug

--- src/js/render-markup.js
+++ src/js/render-markup.js
@@ -5,7 +5,12 @@ import { imgUrl } from './refs';
 export default function renderMarkup(films) {
   const createdElements = films.results
     .map(film => {
+<<<<<<< Updated upstream
       if (film.genre_ids.length === 1 || film.genre_ids.length === 2 || film.genre_ids.length > 2 && film.release_date === '') {
+=======
+      console.log(film.release_date) 
+      if (film.genre_ids.length < 2 && film.genre_ids.length !== 0 && film.release_date === '') {
+>>>>>>> Stashed changes
         const cardFilm = ` 
       <div class="film-card" data-id=${film.id}>
         <img class="film-poster" src="${film.poster_path === null
@@ -21,7 +26,24 @@ export default function renderMarkup(films) {
       </div>`;
         return cardFilm;
 
-      } else if (film.genre_ids.length === 0 && film.release_date === '' ) {
+      } else if (film.genre_ids.length > 2 && film.release_date === '') {
+        const cardFilm = `
+      <div class="film-card" data-id=${film.id}>
+        <img class="film-poster" src="${film.poster_path === null
+          ? `${imgUrl}`
+          : `https://image.tmdb.org/t/p/w500${film.poster_path}`}" 
+          alt="poster">
+        <h2 class="film-title">${film.original_title}</h2>
+        <div class="film-info">
+        <span class="film-details">${film.genre_ids
+          .slice(0, 2)
+          .map(id => genres[id])
+          .join(', ')}, Other</span>
+        </div>
+      </div>`;
+        return cardFilm;
+
+      } else if (film.genre_ids.length === 0 && film.release_date === '') {
         const cardFilm = `
       <div class="film-card" data-id=${film.id}>
         <img class="film-poster" src="${film.poster_path === null
@@ -35,7 +57,7 @@ export default function renderMarkup(films) {
       </div>`;
         return cardFilm;
 
-      } else if (film.genre_ids.length === 0) {
+      } else if (film.genre_ids.length === 0 && film.release_date !== '') {
         const cardFilm = `
       <div class="film-card" data-id=${film.id}>
         <img class="film-poster" src="${film.poster_path === null
@@ -49,7 +71,7 @@ export default function renderMarkup(films) {
       </div>`;
         return cardFilm;
 
-      } else if (film.genre_ids.length === 1 || film.genre_ids.length === 2) {
+      } else if (film.genre_ids.length < 2 && film.genre_ids.length !== 0) {
         const cardFilm = ` 
       <div class="film-card" data-id=${film.id}>
         <img class="film-poster" src="${film.poster_path === null
@@ -65,7 +87,7 @@ export default function renderMarkup(films) {
       </div>`;
         return cardFilm;
 
-      } else if (film.genre_ids.length > 2) {
+      } else if (film.genre_ids.length > 2 && film.genre_ids.length !== 0) {
         const cardFilm = `
       <div class="film-card" data-id=${film.id}>
         <img class="film-poster" src="${film.poster_path === null
