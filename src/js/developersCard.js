e5ea5091ca9ffd71c6fb5017f8bf2ceeb77058fb
InaUA <inanurbaeva@gmail.com>
2023-04-18 14:17:35 +0100

    footmodal

---
+++ src/js/developersCard.js
@@ -0,0 +1,114 @@
+export const developers = [ {
+ 
+    
+    name: 'Igor Borovyk',
+    role: 'Team Lead',
+    image: 'https://ca.slack-edge.com/T04GE8W19NH-U04GXE74BNF-537feda386d3-512',
+    github: 'https://github.com/SMAILe3001',
+
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Inna Nurbaeva',
+    role: 'Scrum Master',
+    image: 'https://avatars.githubusercontent.com/u/115154170?v=4',
+    github: 'https://github.com/InaUA', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Stanislav Pravdiuk',
+    role: 'Full Stack Developer',
+    image: 'https://avatars.githubusercontent.com/u/114864904?v=4',
+    github: 'https://github.com/stanislav-pravdiuk', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Konstantin Le',
+    role: 'Full Stack Developer',
+    image: 'https://ca.slack-edge.com/T04GE8W19NH-U04GZUU9U92-6169b2e6ccaa-512',
+    github: 'https://github.com/klav28', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Oleksii Kryventsov',
+    role: 'Full Stack Developer',
+    image: 'https://avatars.githubusercontent.com/u/114357372?v=4',
+    github: 'https://github.com/Oleksii-Kryventsov', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Danylo Melnyk',
+    role: 'Full Stack Developer',
+    image: 'https://avatars.githubusercontent.com/u/111371263?v=4',
+    github: 'https://github.com/danjkamelnik', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Bogdan Potopnyk',
+    role: 'Full Stack Developer',
+    image: 'https://ca.slack-edge.com/T04GE8W19NH-U04H8LKC21X-dd3432c19b93-512',
+    github: 'https://github.com/Potopnyk', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Oleksandr Kokhno',
+    role: 'Full Stack Developer',
+    image: 'https://ca.slack-edge.com/T04GE8W19NH-U04GUG2BAQ5-8c48da559c4a-512',
+    github: 'https://github.com/Oleksandr-77', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Oleksandr Pavlenko',
+    role: 'Full Stack Developer',
+    image: 'https://avatars.githubusercontent.com/u/114903597?v=4',
+    github: 'https://github.com/Oleksandr-Pavlenko1988', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Ivan Kotov',
+    role: 'Full Stack Developer',
+    image: 'https://avatars.githubusercontent.com/u/70269869?v=4',
+    github: 'https://github.com/BubblePenguin', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+},
+{
+    
+    name: 'Dmytro Lysytsia',
+    role: 'Full Stack Developer',
+    image: 'https://avatars.githubusercontent.com/u/115030615?v=4',
+    github: 'https://github.com/Mr-Foxx', 
+    linkedin: 'https://www.linkedin.com/',
+    instagram: 'https://www.instagram.com/',
+    facebook: 'https://www.facebook.com/',
+}
+
+];
