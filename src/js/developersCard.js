9de4c1a5b23dfe5711637f250170041a6b14cfbb
SMAILe3001 <smaile3001@gmail.com>
2023-04-19 18:56:17 +0300

    fix

--- src/js/developersCard.js
+++ src/js/developersCard.js
@@ -1,8 +1,20 @@
+const user1 = new URL('../images/users/user-1.jpg', import.meta.url);
+const user2 = new URL('../images/users/user-2.jpg', import.meta.url);
+const user3 = new URL('../images/users/user-3.jpg', import.meta.url);
+const user4 = new URL('../images/users/user-4.jpg', import.meta.url);
+const user5 = new URL('../images/users/user-5.jpg', import.meta.url);
+const user6 = new URL('../images/users/user-6.jpg', import.meta.url);
+const user7 = new URL('../images/users/user-7.jpg', import.meta.url);
+const user8 = new URL('../images/users/user-8.jpg', import.meta.url);
+const user9 = new URL('../images/users/user-9.jpg', import.meta.url);
+const user10 = new URL('../images/users/user-10.jpg', import.meta.url);
+const user11 = new URL('../images/users/user-11.jpg', import.meta.url);
+
 export const developers = [
   {
     name: 'Igor Borovyk',
     role: 'Team Lead',
-    image: 'https://ca.slack-edge.com/T04GE8W19NH-U04GXE74BNF-537feda386d3-512',
+    image: user1,
     github: 'https://github.com/SMAILe3001',
 
     linkedin: 'https://www.linkedin.com/',
@@ -12,7 +24,7 @@ export const developers = [
   {
     name: 'Inna Nurbaeva',
     role: 'Scrum Master',
-    image: 'https://avatars.githubusercontent.com/u/115154170?v=4',
+    image: user2,
     github: 'https://github.com/InaUA',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
@@ -21,7 +33,7 @@ export const developers = [
   {
     name: 'Stanislav Pravdiuk',
     role: 'Full Stack Developer',
-    image: 'https://avatars.githubusercontent.com/u/114864904?v=4',
+    image: user3,
     github: 'https://github.com/stanislav-pravdiuk',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
@@ -30,7 +42,7 @@ export const developers = [
   {
     name: 'Konstantin Le',
     role: 'Full Stack Developer',
-    image: 'https://ca.slack-edge.com/T04GE8W19NH-U04GZUU9U92-6169b2e6ccaa-512',
+    image: user4,
     github: 'https://github.com/klav28',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
@@ -39,7 +51,7 @@ export const developers = [
   {
     name: 'Oleksii Kryventsov',
     role: 'Full Stack Developer',
-    image: 'https://avatars.githubusercontent.com/u/114357372?v=4',
+    image: user5,
     github: 'https://github.com/Oleksii-Kryventsov',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
@@ -48,7 +60,7 @@ export const developers = [
   {
     name: 'Danylo Melnyk',
     role: 'Full Stack Developer',
-    image: 'https://avatars.githubusercontent.com/u/111371263?v=4',
+    image: user6,
     github: 'https://github.com/danjkamelnik',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
@@ -57,7 +69,7 @@ export const developers = [
   {
     name: 'Bogdan Potopnyk',
     role: 'Full Stack Developer',
-    image: 'https://ca.slack-edge.com/T04GE8W19NH-U04H8LKC21X-dd3432c19b93-512',
+    image: user7,
     github: 'https://github.com/Potopnyk',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
@@ -66,7 +78,7 @@ export const developers = [
   {
     name: 'Oleksandr Kokhno',
     role: 'Full Stack Developer',
-    image: 'https://ca.slack-edge.com/T04GE8W19NH-U04GUG2BAQ5-8c48da559c4a-512',
+    image: user8,
     github: 'https://github.com/Oleksandr-77',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
@@ -75,7 +87,7 @@ export const developers = [
   {
     name: 'Oleksandr Pavlenko',
     role: 'Full Stack Developer',
-    image: 'https://avatars.githubusercontent.com/u/114903597?v=4',
+    image: user9,
     github: 'https://github.com/Oleksandr-Pavlenko1988',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
@@ -84,7 +96,7 @@ export const developers = [
   {
     name: 'Ivan Kotov',
     role: 'Full Stack Developer',
-    image: 'https://avatars.githubusercontent.com/u/70269869?v=4',
+    image: user10,
     github: 'https://github.com/BubblePenguin',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
@@ -93,7 +105,7 @@ export const developers = [
   {
     name: 'Dmytro Lysytsia',
     role: 'Full Stack Developer',
-    image: 'https://avatars.githubusercontent.com/u/115030615?v=4',
+    image: user11,
     github: 'https://github.com/Mr-Foxx',
     linkedin: 'https://www.linkedin.com/',
     instagram: 'https://www.instagram.com/',
