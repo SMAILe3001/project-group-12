217d3e2b77cb908c5850f17b9c07613bcf8980af
SMAILe3001 <smaile3001@gmail.com>
2023-04-19 13:25:15 +0300

    fix

--- src/js/render-markup_hbs.js
+++ src/js/render-markup_hbs.js
@@ -6,30 +6,30 @@ import filmCardCreate from '../templates/film-card.hbs';
 export default function renderMarkup(films) {
   const createdElements = films.results
     .map(film => {
+      if (film.genre_ids.length > 0) {
+        film.details = film.genre_ids
+          .slice(0, 2)
+          .map(id => genres[id])
+          .join(', ');
 
-        if (film.genre_ids.length > 0) {
-          film.details = film.genre_ids.slice(0,2).map(id => genres[id]).join(", ");
-            
-          if (film.genre_ids.length > 2) {
-            film.details += ", other";
-          }
-
-          if (film.release_date) {
-            film.details += " | " + film.release_date.substr(0, 4);
-          }
-        } else if (film.release_date) {
-            film.details = film.release_date.substr(0, 4);
+        if (film.genre_ids.length > 2) {
+          film.details += ', other';
         }
 
-      const posterSize = (document.body.clientWidth <= 768)
-        ? 'w342'
-        : 'w500';
+        if (film.release_date) {
+          film.details += ' | ' + film.release_date.substr(0, 4);
+        }
+      } else if (film.release_date) {
+        film.details = film.release_date.substr(0, 4);
+      }
+
+      const posterSize = document.body.clientWidth <= 768 ? 'w185' : 'w500';
 
       film.poster_url = film.poster_path
-          ? `https://image.tmdb.org/t/p/${posterSize}${film.poster_path}`
-          : `${imgUrl}`;
+        ? `https://image.tmdb.org/t/p/${posterSize}${film.poster_path}`
+        : `${imgUrl}`;
 
-        return filmCardCreate(film);
+      return filmCardCreate(film);
     })
     .join('');
 
